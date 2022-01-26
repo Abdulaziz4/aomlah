1d65de12bd7c1f5e340a2b2f4ed6e7654ac5e84f
Zyad07h <zyad.a2000@gmail.com>
2022-01-26 17:43:54 +0300

    modifiet market_view

--- lib/ui/views/market/market_view.dart
+++ lib/ui/views/market/market_view.dart
@@ -6,7 +6,7 @@ class MarketView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Market ....")),
+      body: Center(child: Text("Market")),
     );
   }
 }
