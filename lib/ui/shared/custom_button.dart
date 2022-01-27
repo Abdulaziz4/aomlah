46f884a2ce9f28fdef755e11b6d2f1a7a06437b3
Abdulaziz Alqahtani <h-xa2012@hotmail.com>
2022-01-27 12:16:22 +0300

    Adding Custom Button

---
+++ lib/ui/shared/custom_button.dart
@@ -0,0 +1,45 @@
+import 'package:aomlah/core/app/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final void Function() onPressed;
+  final String text;
+  final Color? color;
+  final TextStyle? textStyle;
+  final double? height;
+  final double? width;
+
+  const CustomButton({
+    required this.onPressed,
+    required this.text,
+    this.height,
+    this.textStyle,
+    this.width,
+    this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(width ?? 120, width ?? 35),
+        primary: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            7,
+          ), // <-- Radius
+        ),
+        elevation: 3,
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: textStyle ??
+            Constants.smallText.copyWith(
+              color: Colors.grey[200],
+            ),
+      ),
+    );
+  }
+}
--- lib/ui/views/offers_discovery/offers_discovery_view.dart
+++ lib/ui/views/offers_discovery/offers_discovery_view.dart
@@ -1,6 +1,9 @@
 import 'package:aomlah/core/app/app.locator.dart';
 import 'package:aomlah/core/app/app.router.dart';
+import 'package:aomlah/core/app/utils/constants.dart';
+import 'package:aomlah/ui/shared/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class OffersDiscoveryView extends StatelessWidget {
@@ -34,18 +37,83 @@ class OffersDiscoveryView extends StatelessWidget {
             ],
           ),
         ),
-        body: const OfferBody(),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: TabBarView(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: 200,
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Constants.black2dp,
+                      boxShadow: Constants.shadow,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            SvgPicture.asset("assets/icons/profile-icon.svg"),
+                            SizedBox(width: 5),
+                            Text("عبدالعزيز"),
+                            Spacer(),
+                            Text("2021/5/1")
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                buildInfoItem("السعر", "121,41241,124 ر.س"),
+                                buildInfoItem("الكمية", "0.1245 BTC"),
+                              ],
+                            ),
+                            Column(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                buildInfoItem("الحد الادنى", "2000 ر.س"),
+                                CustomButton(
+                                  onPressed: () {},
+                                  text: "شراء",
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Icon(Icons.deck),
+            ],
+          ),
+        ),
       ),
     );
   }
-}
 
-class OfferBody extends StatelessWidget {
-  const OfferBody({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text("Offs ....."),
+  Widget buildInfoItem(String title, String subtitle) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: Constants.smallText.copyWith(
+            color: Constants.darkBlue,
+          ),
+        ),
+        Text(
+          subtitle,
+          style: Constants.mediumText,
+        ),
+      ],
     );
   }
 }
