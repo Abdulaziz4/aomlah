508d92d1098d9760a9958163c2df1db8ecc3d97b
abdulaziz1928 <abdulaziz1928@gmail.com>
2022-01-27 04:58:44 +0300

    Adding Create Offer AppBar

---
+++ lib/ui/views/create_offer/create_offer_buy.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateOfferBuy extends StatelessWidget {
+  const CreateOfferBuy({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Text("buuuuuy");
+  }
+
+}
--- lib/ui/views/create_offer/create_offer_view.dart
+++ lib/ui/views/create_offer/create_offer_view.dart
@@ -1,3 +1,6 @@
+import 'package:aomlah/core/app/utils/constants.dart';
+import 'package:aomlah/ui/views/create_offer/create_offer_buy.dart';
+import 'package:aomlah/ui/views/create_offer/create_offer_sell.dart';
 import 'package:flutter/material.dart';
 
 class CreateOfferView extends StatelessWidget {
@@ -5,12 +8,39 @@ class CreateOfferView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Offeeeers .....",
-        ),
-      ),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'انشاء اعلان',
+              style: TextStyle(
+                  color: Constants.darkBlue, fontWeight: FontWeight.bold),
+            ),
+            centerTitle: true,
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'بيع',),
+                Tab(text: 'شراء',)
+              ],
+            ),
+
+          ),
+          body: CreateOfferBody()),
     );
+  }
+}
+
+class CreateOfferBody extends StatelessWidget {
+  const CreateOfferBody({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TabBarView(children: [
+      //SellView
+      CreateOfferSell(),
+      //BuyView
+      CreateOfferBuy()
+      
+    ]);
   }
 }
