24b8db6cc6c75a51454c814f5ff0edc162ed3b55
Abdulaziz Alqahtani <h-xa2012@hotmail.com>
2022-01-27 12:28:20 +0300

    Complete Offers Discovery

--- lib/ui/views/offers_discovery/offers_discovery_view.dart
+++ lib/ui/views/offers_discovery/offers_discovery_view.dart
@@ -1,9 +1,7 @@
 import 'package:aomlah/core/app/app.locator.dart';
 import 'package:aomlah/core/app/app.router.dart';
-import 'package:aomlah/core/app/utils/constants.dart';
-import 'package:aomlah/ui/shared/custom_button.dart';
+import 'package:aomlah/ui/views/offers_discovery/components/offer_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class OffersDiscoveryView extends StatelessWidget {
@@ -14,19 +12,16 @@ class OffersDiscoveryView extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        // appBar: AppBar(
-        //   actions: [
-        //     IconButton(
-        //       onPressed: () {
-        //         locator<NavigationService>().navigateTo(Routes.createOfferView);
-        //       },
-        //       icon: Icon(Icons.add),
-        //     ),
-        //   ],
-        //   automaticallyImplyLeading: false,
-        // ),
         appBar: AppBar(
           title: Text("التداول"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                locator<NavigationService>().navigateTo(Routes.createOfferView);
+              },
+              icon: Icon(Icons.add),
+            ),
+          ],
           automaticallyImplyLeading: false,
           bottom: TabBar(
             unselectedLabelColor: Colors.grey,
@@ -37,83 +32,43 @@ class OffersDiscoveryView extends StatelessWidget {
             ],
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: TabBarView(
-            children: [
-              Column(
-                children: [
-                  Container(
-                    height: 200,
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Constants.black2dp,
-                      boxShadow: Constants.shadow,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            SvgPicture.asset("assets/icons/profile-icon.svg"),
-                            SizedBox(width: 5),
-                            Text("عبدالعزيز"),
-                            Spacer(),
-                            Text("2021/5/1")
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                buildInfoItem("السعر", "121,41241,124 ر.س"),
-                                buildInfoItem("الكمية", "0.1245 BTC"),
-                              ],
-                            ),
-                            Column(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                buildInfoItem("الحد الادنى", "2000 ر.س"),
-                                CustomButton(
-                                  onPressed: () {},
-                                  text: "شراء",
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+        body: TabBarView(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: const [
+                  OfferCard(),
+                  OfferCard(),
+                  OfferCard(),
+                  OfferCard(),
+                  OfferCard(),
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: const [
+                  OfferCard(
+                    isBuy: false,
+                  ),
+                  OfferCard(
+                    isBuy: false,
+                  ),
+                  OfferCard(
+                    isBuy: false,
+                  ),
+                  OfferCard(
+                    isBuy: false,
+                  ),
+                  OfferCard(
+                    isBuy: false,
                   ),
                 ],
               ),
-              Icon(Icons.deck),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-    );
-  }
-
-  Widget buildInfoItem(String title, String subtitle) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          title,
-          style: Constants.smallText.copyWith(
-            color: Constants.darkBlue,
-          ),
-        ),
-        Text(
-          subtitle,
-          style: Constants.mediumText,
-        ),
-      ],
     );
   }
 }
