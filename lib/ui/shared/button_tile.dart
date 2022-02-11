24fa99433c890dce9a64009bd2372c536b1e3540
abdulaziz1928 <abdulaziz1928@gmail.com>
2022-02-11 19:28:42 +0300

    new branch

---
+++ lib/ui/shared/button_tile.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+import 'package:aomlah/core/app/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class ButtonTile extends StatelessWidget {
+  final String text;
+  final void Function() onPressed;
+  const ButtonTile({
+    Key? key,
+    required this.onPressed,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: 70,
+        color: Constants.black2dp,
+        margin: const EdgeInsets.symmetric(vertical: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              text,
+              style: Constants.mediumText,
+            ),
+            Transform.rotate(
+              angle: pi,
+              child: Icon(Icons.arrow_back_ios),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/views/settings/settings_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettingsHome extends StatelessWidget {
+  const SettingsHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
