08cf6b02e0dad0d22980e413c9c53613ed4d7cd0
Abdulaziz Alqahtani <h-xa2012@hotmail.com>
2022-02-24 00:22:08 +0300

    Adding Offers Date

--- lib/ui/views/offers_discovery/components/offer_card.dart
+++ lib/ui/views/offers_discovery/components/offer_card.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart' as intl;
 
 class OfferCard extends StatelessWidget {
   final Offer offer;
@@ -50,7 +51,13 @@ class OfferCard extends StatelessWidget {
                 SizedBox(width: 5),
                 Text(offer.ownerName ?? ""),
                 Spacer(),
-                Text("2021/5/1")
+                Text(
+                  intl.DateFormat.yMMMd().add_jm().format(
+                        offer.createAt!,
+                      ),
+                  textDirection: TextDirection.ltr,
+                  style: Constants.verySmallText.copyWith(color: Colors.grey),
+                )
               ],
             ),
             SizedBox(height: 5),
