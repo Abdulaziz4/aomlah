44e520fe8e4aaa8e3712ae59f70f7ed65321ff65
Abdulaziz Alqahtani <h-xa2012@hotmail.com>
2022-03-01 18:55:07 +0300

    Creating sell trade

--- lib/ui/views/trading/trader/sell_coin/sell_coin_overview_view.dart
+++ lib/ui/views/trading/trader/sell_coin/sell_coin_overview_view.dart
@@ -1,9 +1,11 @@
 import 'package:aomlah/core/app/utils/constants.dart';
+import 'package:aomlah/core/models/bitcoin.dart';
 import 'package:aomlah/core/models/offer.dart';
 import 'package:aomlah/ui/views/trading/components/offer_purchase_overview.dart';
 import 'package:aomlah/ui/views/trading/components/payment_window.dart';
 import 'package:aomlah/ui/views/trading/trader/sell_coin/viewmodels/sell_coin_overview_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:stacked/stacked.dart';
 
 class SellCoinOverviewView extends StatelessWidget {
@@ -12,6 +14,8 @@ class SellCoinOverviewView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final btc = Provider.of<Bitcoin>(context);
+
     return ViewModelBuilder<SellCoinOverviewViewModel>.reactive(
         viewModelBuilder: () => SellCoinOverviewViewModel(),
         builder: (context, viewmodel, _) {
@@ -22,7 +26,9 @@ class SellCoinOverviewView extends StatelessWidget {
                 formKey: viewmodel.formKey,
                 offer: offer,
                 onAmountSaved: viewmodel.setAmount,
-                onSubmit: viewmodel.submit,
+                onSubmit: () {
+                  viewmodel.submit(btc.price, offer.offerID);
+                },
                 onSelectBankAccount: viewmodel.selectBankAccount,
                 selectedBank: viewmodel.bankAccount,
                 errorMessage: viewmodel.errorMessage,
--- lib/ui/views/trading/trader/sell_coin/viewmodels/sell_coin_overview_viewmodel.dart
+++ lib/ui/views/trading/trader/sell_coin/viewmodels/sell_coin_overview_viewmodel.dart
@@ -1,14 +1,20 @@
 import 'package:aomlah/core/app/app.locator.dart';
 import 'package:aomlah/core/app/app.router.dart';
+import 'package:aomlah/core/app/utils/uuid_helper.dart';
+import 'package:aomlah/core/enums/trade_state.dart';
 import 'package:aomlah/core/models/bank_account.dart';
+import 'package:aomlah/core/models/trade.dart';
 import 'package:aomlah/core/services/supabase_service.dart';
+import 'package:aomlah/core/services/trading_service.dart';
+import 'package:aomlah/core/services/user_service.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class SellCoinOverviewViewModel extends BaseViewModel {
   final _navService = locator<NavigationService>();
-  final _supabaseService = locator<SupabaseService>();
+  final _tradingService = locator<TradingService>();
+  final _userService = locator<UserService>();
 
   final formKey = GlobalKey<FormState>();
 
@@ -18,7 +24,7 @@ class SellCoinOverviewViewModel extends BaseViewModel {
 
   String? errorMessage;
 
-  void submit() {
+  void submit(double price, String offerId) {
     resetBankError();
 
     bool isValid = formKey.currentState!.validate();
@@ -33,7 +39,16 @@ class SellCoinOverviewViewModel extends BaseViewModel {
 
     formKey.currentState!.save();
     setBusy(true);
-
+    final trade = Trade(
+      tradeId: UuidHelper.generate(),
+      amount: (amount / 3.75) / price,
+      offerId: offerId,
+      status: TradeStatus.awaiting_payment,
+      traderId: _userService.user.profileId,
+      price: price,
+      bankIban: bankAccount!.iban,
+    );
+    _tradingService.createTrade(trade);
     setBusy(false);
     _navService.navigateTo(Routes.traderSellCoinView);
   }
