05049148d9cc1d848b69faeaec9d20ae0c07eaec
Abdulaziz Alqahtani <h-xa2012@hotmail.com>
2022-05-03 16:50:38 +0300

    Setup unit testing

---
+++ tests/mocks/mocks.dart
@@ -0,0 +1,7 @@
+import 'package:aomlah/core/services/user_service.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class MockUserService extends Mock implements UserService {}
+
+class MockNavigationService extends Mock implements NavigationService {}
